e128372df259897ec6d7908ae6eea814b38f9926
umaira-shaheen <umairaraja01@gmail.com>
2023-05-02 21:04:21 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-500">
+      
+      <div class="container">
+
+
+        <button class="bg-blue-500 hover:bg-blue-200 text-pink font-bold py-2 px-4 rounded">Find Friend</button>
+        <button class="btn btn-blue">Find Groups</button>
+      
+        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 roundedrelative my-5" role="alert">
+         
+          <strong class="font-bold">Alert!</strong>
+          <span class="block sm-inline">Please Update your password</span>
+
+        </div>
+
+      </div>
+      <div class="flex">
+      <div class="max-w-sm mx-auto flex p-6 bg-white rounded shadow-xl">
+        <div class="flex-shrink-0">
+          <img src="https://i.pinimg.com/736x/92/a6/2f/92a62f0221f58fe503a15fcb13f5c107--tom-jerry-movies-.jpg" alt="cartoon" class="h-40 w-40"/>
+          
+        </div>
+        <div class="ml-6 pt-1">
+          <h4 class="text-xl text-gray-900">Hello!! I am Tom</h4>
+        </div>
+
+      </div>
+      <div class="max-w-sm mx-auto flex p-6 bg-white rounded shadow-xl">
+        <div class="flex-shrink-0">
+          <img src="https://i.pinimg.com/originals/f4/ed/18/f4ed18e4f8c07a1688153de9daa5bd6c.jpg" alt="cartoon" class="h-40 w-40"/>
+          
+        </div>
+        <div class="ml-6 pt-1">
+          <h4 class="text-xl text-gray-900">Hello!! I am Jeery</h4>
+        </div>
+
+      </div>
+
+      </div>
+      
+
+
+      
     </div>
+
+
+
+
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+
+  ],
+  theme: {
+    container:{
+      padding:"2rem",
+      center:true
+  
+    }
+  },
+  
+  plugins: [],
+}
+
